b600407fcf6bbe7c9fc34168f5f9e93f650520da
Daniil Halushka <popolzen22@gmail.com>
2025-01-09 19:55:38 +0300

    feat: add custom session Create ChatSession and add intercept in Security plugin

--- ktorChat/src/main/kotlin/com/halushka/chat/plugins/Security.kt
+++ ktorChat/src/main/kotlin/com/halushka/chat/plugins/Security.kt
@@ -1,17 +1,22 @@
 package com.halushka.chat.plugins
 
+import com.halushka.chat.session.ChatSession
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
+import io.ktor.util.*
 import kotlinx.serialization.Serializable
 
 fun Application.configureSecurity() {
     install(Sessions) {
-        cookie<MySession>("MY_SESSION") {
-            cookie.extensions["SameSite"] = "lax"
+        cookie<ChatSession>("SESSION")
+    }
+
+    intercept(ApplicationCallPipeline.Plugins) {
+        if (call.sessions.get<ChatSession>() == null) {
+            val username = call.parameters["username"] ?: "Guest"
+            call.sessions.set(ChatSession(username, generateNonce()))
         }
     }
 }
-@Serializable
-data class MySession(val count: Int = 0)
