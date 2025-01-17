f5068f08dd2b5f2a3e615f58d7c12c8396ac8b00
Daniil Halushka <popolzen22@gmail.com>
2025-01-17 19:24:21 +0300

    feature: create models
    Create MessageDto with mapper and Message model

---
+++ android/app/src/main/java/com/halushka/ktorchat/data/remote/dto/MessageDto.kt
@@ -0,0 +1,27 @@
+package com.halushka.ktorchat.data.remote.dto
+
+import com.halushka.ktorchat.domain.model.Message
+import kotlinx.serialization.Serializable
+import java.text.DateFormat
+import java.util.Date
+
+@Serializable
+data class MessageDto(
+    val text: String,
+    val timestamp: Long,
+    val username: String,
+    val id: String
+) {
+    fun mapToMessage(): Message {
+        val date = Date(timestamp)
+        val formattedDate = DateFormat
+            .getDateInstance(DateFormat.DEFAULT)
+            .format(date)
+
+        return Message(
+            text = text,
+            formattedTime = formattedDate,
+            username = username
+        )
+    }
+}
